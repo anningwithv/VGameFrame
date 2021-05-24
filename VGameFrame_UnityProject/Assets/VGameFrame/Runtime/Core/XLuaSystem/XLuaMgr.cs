5c7a8142b5e2c4c60e36af8f59afca68716f8eec
zhangwei_modooplay <wei.zhang@modooplay.com>
2021-05-24 15:23:51 +0800

    XLua

---
+++ VGameFrame_UnityProject/Assets/VGameFrame/Runtime/Core/XLuaSystem/XLuaMgr.cs
@@ -0,0 +1,39 @@
+﻿//-------------------------------------------------------
+//  Desc:        Framework For Game Develop with Unity3d 
+//  Copyright:   Copyright (C) 2021. All rights reserved. 
+//  Website:     https://github.com/anningwithv/VGameFramework. 
+//  Author:      V 
+//-------------------------------------------------------
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using XLua;
+
+namespace VGameFramework
+{
+	public class XLuaMgr : TSingleton<XLuaMgr>, IManager
+    {
+        private LuaEnv m_LuaEnv = null;
+
+        #region IManager
+
+        public void OnInit()
+        {
+            m_LuaEnv = new LuaEnv();
+            m_LuaEnv.DoString("XLuaTest");
+        }
+
+        public void OnUpdate()
+        {
+        }
+
+        public void OnDestroy()
+        {
+        }
+
+        #endregion
+
+    }
+
+}
